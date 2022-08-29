b2b12d92d005f28506b71db3ef190b1d6b9667e3
Jonasson Oscar <oscjjonasson@gmail.com>
2022-08-29 14:38:11 +0300

    dispatches

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import Button from './Components/Button/Button.jsx';
 import Circle from './Components/Circle/Circle';
 import classes from './App.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCount } from './features/counter/CounterSlice';
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount,
+  decrementByAmount,
+} from './features/counter/CounterSlice';
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
+  // same as CounterSlice export selectCount
+  const count = useSelector(selectCount);
+  // const count = useSelector((state) => state.counter.value);
+
+  const dispatch = useDispatch();
+
   return (
     <div className={classes.container}>
       <Circle number={count} />
       <div className={classes.buttons}>
-        <Button text="Decrease 5" onClick="" />
-        <Button text="Decrease 1" onClick="" />
-        <Button text="Reset" onClick="" />
-        <Button text="Increase 1" onClick="" />
-        <Button text="Increase 5" onClick="" />
+        <Button
+          text="Decrease 5"
+          onClick={() => {
+            dispatch(decrementByAmount(5));
+          }}
+        />
+        <Button
+          text="Decrease 1"
+          onClick={() => {
+            dispatch(decrement());
+          }}
+        />
+        <Button
+          text="Reset"
+          onClick={() => {
+            dispatch(reset());
+          }}
+        />
+        <Button
+          text="Increase 1"
+          onClick={() => {
+            dispatch(increment());
+          }}
+        />
+        <Button
+          text="Increase 5"
+          onClick={() => {
+            dispatch(incrementByAmount(5));
+          }}
+        />
       </div>
     </div>
   );
--- src/features/counter/CounterSlice.js
+++ src/features/counter/CounterSlice.js
@@ -11,8 +11,29 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.value += 1;
     },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+    decrement: (state) => {
+      state.value > 0 ? (state.value -= 1) : (state.value = state.value);
+    },
+    decrementByAmount: (state, action) => {
+      state.value >= action.payload
+        ? (state.value -= action.payload)
+        : (state.value = state.value);
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const {
+  increment,
+  incrementByAmount,
+  reset,
+  decrement,
+  decrementByAmount,
+} = counterSlice.actions;
+export const selectCount = (state) => state.counter.value;
 export default counterSlice.reducer;
