f5e291adad50c29ba72666e181760bfe233ee648
Jonasson Oscar <oscjjonasson@gmail.com>
2022-08-29 13:48:30 +0300

    slice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Button from './Components/Button/Button.jsx';
 import Circle from './Components/Circle/Circle';
 import classes from './App.module.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
   return (
     <div className={classes.container}>
-      <Circle number="0" />
+      <Circle number={count} />
       <div className={classes.buttons}>
         <Button text="Decrease 5" onClick="" />
         <Button text="Decrease 1" onClick="" />
